ea9ac5824c5ef4784d2cfea2f48c657de2da289a
dc-p8 <cardinal.dorian@gmail.com>
2018-12-13 17:51:53 +0100

    initial commit

---
+++ dl/helloworld/helloworld.mk
@@ -0,0 +1,16 @@
+
+HELLOWORLD_SOURCE = helloworld.js
+HELLOWORLD_SITE = https://google.com
+
+define HELLOWORLD_EXTRACT_CMDS
+	cp $(DL_DIR)/$(HELLOWORLD_SOURCE) $(@D)
+	read -p "tata..."
+endef
+
+define HELLOWORLD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/$(HELLOWORLD_SOURCE) \
+		$(TARGET_DIR)/opt/helloworld.js
+	read -p "toto..."
+endef
+
+$(eval $(generic-package))
